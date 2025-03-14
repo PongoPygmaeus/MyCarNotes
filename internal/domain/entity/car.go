9fe1133583544fcb1455a7f747fa67f3f823390a
gabrielsouza95 <alves95gabriel@gmail.com>
2025-03-13 23:32:12 -0300

    feat: adding some properties for the car and trying to do a enum for the fuel type

--- internal/domain/entity/car.go
+++ internal/domain/entity/car.go
@@ -1,1 +1,18 @@
 package entity
+
+type Car struct {
+	Name string
+	Manufacturer string
+	Model string
+	Year string
+	ModelYear string
+	FuelType Fuel
+}
+
+type Fuel int
+
+const (
+	E100 Fuel = iota
+	E60
+	E30
+)
